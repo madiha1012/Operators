52960069e2d7bae97209d727122c6fc691ebc8d5
Madiha Khan <60982372+madiha1012@users.noreply.github.com>
2022-09-25 19:20:35 +0500

    Add files via upload

---
+++ equality_and_relational_operators.dart
@@ -0,0 +1,16 @@
+void main() {
+  print("Equality or relational Operators");
+  print("================================");
+  
+  int num1 = 10;
+  int num2 = 4;
+  print(num1 < num2);
+  print(num2 > num2);
+  print(num2 >= num2);
+  print(num2 <= num2);
+  print(num2 == num2);
+  print(num2 != num2);
+  
+  
+ 
+}
