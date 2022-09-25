773f4c5806264f8d88cc270e25512f46e65720f1
Madiha Khan <60982372+madiha1012@users.noreply.github.com>
2022-09-25 18:50:38 +0500

    Add files via upload

---
+++ ArithmeticOperator.dart
@@ -0,0 +1,13 @@
+void main() {
+  print("Arithmatic Operators Part01");
+  print("================================");
+  
+  int num1 = 10;
+  int num2 = 4;
+  print("Addition: ${num1+num2}");
+  print("subtraction: ${num1-num2}");
+  print("multiplication: ${num1*num2}");
+  print("division: ${num1/num2}");
+  print("divide return int,Tilda: ${num1~/num2}");
+  print("Remainder: ${num1%num2}");
+}
